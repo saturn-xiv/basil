b8a2846c9020bc3b10d8c50351f28c5508a289bd
Jeremy Zheng <jitang.zheng@gmail.com>
2026-06-25 04:15:12 +0000

    :wrench: add build assets & app

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,24 +3,38 @@
 set -e
 
 export WORK_DIR=$PWD
-export PACKAGE=basil-$(git describe --tags --always --dirty --first-parent)-$(uname -m)
+export PACKAGE=basil-$(git describe --tags --always --dirty --first-parent)
 export TARGET=$WORK_DIR/tmp/$PACKAGE
 
 if [ -d $TARGET ]
 then
     rm -r $TARGET
 fi
-mkdir -p $TARGET/bin
 
 cd $WORK_DIR/agent/
-make
-cp basil-agent $TARGET/bin/
 
 cd $WORK_DIR/
-cargo build --release
-cp target/release/basil $TARGET/bin/
-npm install
-cp -r node_modules assets LICENSE README.md $TARGET/
+platforms=("x86_64" "aarch64")
+for p in "${platforms[@]}"; do
+    cargo build --release --target $p-unknown-linux-gnu
+    mkdir -p $TARGET/$p/bin
+    cp -v target/$p-unknown-linux-gnu/release/basil $TARGET/$p/bin/
+done
+
+
+cd $WORK_DIR/
+if [ ! -d node_modules ]
+then
+    npm install
+fi
+assets=("bulma/css/bulma.min.css")
+for n in "${assets[@]}"; do
+    t=$TARGET/node_modules/$(dirname $n)
+    mkdir -p $t
+    cp -rv node_modules/$n $t/
+done
+
+cp -rv assets LICENSE README.md $TARGET/
 
 XZ_OPT=-9 tar -cJf $TARGET.tar.xz  -C $TARGET .
 echo "done($PACKAGE)."
