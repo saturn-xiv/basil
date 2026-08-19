8bd1ac68541ee050b609bf18491ca493a2825773
Jeremy Zheng <jitang.zheng@gmail.com>
2026-08-19 04:04:00 +0000

    :fire: remove assets folder

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 ## Usage
 
+    ```bash
+    git clone https://github.com/saturn-xiv/basil.git $HOME/workspace/basil
+    cd $HOME/workspace/basil/
+    ./build.sh
+    ```
--- scripts/build.sh
+++ scripts/build.sh
@@ -4,11 +4,29 @@ set -e
 
 export WORK_DIR=$PWD
 export PACKAGE=basil-$(git describe --tags --always --dirty --first-parent)
-export TARGET=$WORK_DIR/tmp/$PACKAGE
+export TARGET_DIR=$WORK_DIR/tmp/$PACKAGE
 
-if [ -d $TARGET ]
+source /etc/os-release
+
+if [ -f $TARGET_DIR/$PACKAGE.md5 ]
 then
-    rm -r $TARGET
+    echo "building $PACKAGE already exists"
+    exit 0
+fi
+
+if [ -f $TARGET_DIR/$PACKAGE.tar.xz ]
+then
+    rm $TARGET_DIR/$PACKAGE.tar.xz
+fi
+
+if [ -d $TARGET_DIR/$PACKAGE ]
+then
+    rm -r $TARGET_DIR/$PACKAGE
+fi
+
+if [ "$ID" = "ubuntu" ]
+then
+    apt install -y libboost-all-dev libsnmp-dev libcurl4-openssl-dev
 fi
 
 cd $WORK_DIR/
@@ -19,32 +37,30 @@ cmake -Wno-dev -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=clang -DCMAKE_CXX_C
     -B $LAVENDER_BUILD_DIR -S $WORK_DIR/lavender \
     -G Ninja
 cmake --build $LAVENDER_BUILD_DIR
-mkdir -p $TARGET/$(uname -m)/bin
-cp $LAVENDER_BUILD_DIR/lavender $TARGET/$(uname -m)/bin/
+mkdir -p $TARGET_DIR/$PACKAGE/$(uname -m)/bin
+cp $LAVENDER_BUILD_DIR/lavender $TARGET_DIR/$PACKAGE/$(uname -m)/bin/
 
 cd $WORK_DIR/
-platforms=("x86_64" "aarch64")
+declare -a platforms=("x86_64" "aarch64" "riscv64gc")
 for p in "${platforms[@]}"; do
     cargo build --release --target $p-unknown-linux-gnu
-    mkdir -p $TARGET/$p/bin
-    cp -v target/$p-unknown-linux-gnu/release/basil $TARGET/$p/bin/
+    mkdir -p $TARGET_DIR/$PACKAGE/$p/bin
+    cp -v target/$p-unknown-linux-gnu/release/basil $TARGET_DIR/$PACKAGE/$p/bin/
 done
 
 
-cd $WORK_DIR/
+cd $WORK_DIR/dashboard/
 if [ ! -d node_modules ]
 then
     npm install
 fi
-assets=("bulma/css/bulma.min.css")
-for n in "${assets[@]}"; do
-    t=$TARGET/node_modules/$(dirname $n)
-    mkdir -p $t
-    cp -rv node_modules/$n $t/
-done
+npm run build
+cp -r dist $TARGET_DIR/$PACKAGE/dashboard
 
-cp -rv assets LICENSE README.md $TARGET/
+cp -r LICENSE README.md $TARGET_DIR/$PACKAGE/
 
-XZ_OPT=-9 tar -cJf $TARGET.tar.xz  -C $TARGET .
+XZ_OPT=-9 tar -cJf $TARGET_DIR/$PACKAGE.tar.xz --remove-files -C $TARGET_DIR/$PACKAGE .
+md5sum $TARGET_DIR/$PACKAGE.tar.xz > $TARGET_DIR/$PACKAGE.md5
+
 echo "done($PACKAGE)."
 exit 0
