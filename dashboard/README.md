dc0fa84a3867e041017174ff8f025b65830d0af5
Jeremy Zheng <jitang.zheng@gmail.com>
2026-08-19 03:38:46 +0000

    :tada: create dashboard project

---
+++ dashboard/README.md
@@ -0,0 +1,8 @@
+# DASHBOARD
+
+## Development
+
+```bash
+# http://localhost:3000
+npm run dev -- --port 3000
+```
---
+++ dashboard/vite.config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import babel from "@rolldown/plugin-babel";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), babel({ presets: [reactCompilerPreset()] })],
+  base: "/my/",
+  server: {
+    proxy: {
+      "/graphql": "http://localhost:4000",
+      "/api": "http://localhost:4000",
+    },
+  },
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
+});
